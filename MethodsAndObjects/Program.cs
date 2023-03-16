f88c98f99bedbe0cbe2f5f12273ea6844106eab5
Arieleon25 <Arieleon25@yahoo.com>
2023-03-16 17:42:02 -0400

    Add project files.

---
+++ MethodsAndObjects/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee= new Employee();
+
+            employee.FirstName = "Sample";
+            Console.WriteLine(employee.FirstName);
+            employee.LastName = "Student";
+            Console.WriteLine(employee.LastName);
+            employee.SayName();
+
+            
+            Console.ReadLine();
+        }
+    }
+}
